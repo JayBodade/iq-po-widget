df4e19cf7138bc1ea5d267cfbdc4fd9b31d380c4
JayBodade <intern@incquet.com>
2024-08-07 02:41:47 +0530

    added env variables

--- iq_pos_vue3/src/firebase/index.js
+++ iq_pos_vue3/src/firebase/index.js
@@ -2,16 +2,23 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import "firebase/auth";
 
-
+const {VUE_APP_APIKEY,
+  VUE_APP_AUTHDOMAIN,
+  VUE_APP_PROJECTID,
+  VUE_APP_STORAGEBUCKET,
+  VUE_APP_SENDERID,
+  VUE_APP_APPID,
+  VUE_APP_MEASUREMENTID } = process.env
+  console.log("this is process",process.env);
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjPk1hWALrFDytEEeXN2udVZU4GHIrr4E",
-  authDomain: "silent-complex-381416.firebaseapp.com",
-  projectId: "silent-complex-381416",
-  storageBucket: "silent-complex-381416.appspot.com",
-  messagingSenderId: "396369508726",
-  appId: "1:396369508726:web:f642cf86a7a2e91e8ce257",
-  measurementId: "G-2Q8KGMQEQM"
+  apiKey: VUE_APP_APIKEY,
+  authDomain: VUE_APP_AUTHDOMAIN,
+  projectId: VUE_APP_PROJECTID,
+  storageBucket: VUE_APP_STORAGEBUCKET,
+  messagingSenderId: VUE_APP_SENDERID,
+  appId: VUE_APP_APPID,
+  measurementId: VUE_APP_MEASUREMENTID
   };
   
 
